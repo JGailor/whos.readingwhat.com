d153280181f4214874a00b77fa6a5ec427ce6b16
Jeremy Gailor <jeremy@infinitecube.com>
2008-09-26 08:46:41 -0700

    Moving the project from merb to rails

--- app/controllers/application.rb
+++
@@ -1,7 +0,0 @@
-class Application < Merb::Controller
-  before :set_title
-  
-  def set_title
-    @title = "whos.readingwhat.com"
-  end
-end
--- app/models/user.rb
+++
@@ -1,9 +0,0 @@
-class User
-  include DataMapper::Resource
-
-  property :id,             Integer,  :serial => true
-  property :email,          String,   :nullable => false
-  property :password,       String,   :nullable => false
-  property :created_at,     DateTime, :nullable => false
-  property :last_logged_in, DateTime, :nullable => false
-end

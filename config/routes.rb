93cb6e6efe1c8f8d0fcf39731ef08fd0e1f08112
Jeremy Gailor <jeremy@infinitecube.com>
2008-09-26 09:15:06 -0700

    Added images and stylesheets from the html layout Haml'ized the project

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
-  map.root :controller => :index, :action => :index
+  map.root :controller => 'home', :action => 'index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
